a97ee3efe32bc605bf136a2e7806f9d1717790b5
Faisal Ayash <faisalayash@mile.app>
2025-05-04 09:01:41 +0700

    fix: unvailable data detail anime

--- src/components/Stat/AnimeStat.jsx
+++ src/components/Stat/AnimeStat.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 
 const StatSkeleton = () => {
@@ -25,36 +26,45 @@ const StatSkeleton = () => {
     )
 }
 
+const Score = ({score = 5}) => {
+
+    const scoreClass = clsx('text-xl p-2 text-white rounded aspect-square text-center flex items-center', score <= 3 && "bg-rose-500", score > 3 && score < 7  && "bg-orange-500", score >= 7 && 'bg-emerald-500')
+    return <div className={scoreClass}>
+        {score ? score : "N/A"}
+    </div>
+}
+
 const AnimeStat = ({data, loading = true}) => {
     return (
         loading || !data ? <StatSkeleton /> :
         <div className='flex gap-8 flex-col sm:flex-row'>
             <div className='flex flex-col items-center px-8 sm:border-r border-gray-300'>
                 <span className='text-sm'>Score</span>
-                <div className='text-xl p-2 bg-blue-500 text-white rounded'>
+                <Score score={data.score}/>
+                {/* <div className='text-xl p-2 bg-blue-500 text-white rounded'>
                     {data.score ? data.score : "N/A"}
-                </div>
+                </div> */}
                 <span className='text-xs text-gray-400'>{data.scored_by ? data.scored_by : "N/A"} users</span>
 
             </div>
             
             <div className='flex flex-col grow justify-around gap-4 sm:gap-1'>
-                <div className='flex gap-8'>
-                    <h3 className='text-lg'>
+                <div className='flex gap-8 text-lg'>
+                    <h3>
                         Ranked <span className='font-semibold'>#{data.rank}</span>
                     </h3>
-                    <h3 className='text-lg'>
+                    <h3>
                         Popularity <span className='font-semibold'>#{data.popularity}</span>
                     </h3>
-                    <h3 className='text-lg'>
+                    <h3>
                         Members <span className='font-semibold'>{data.members}</span>
                     </h3>
                 </div>
                 <div className='flex gap-4 text-sm justify-center sm:justify-start'>
                     {data.season && data.year ? <small>{data.season} {data.year}</small> :
-                    <small>Unknown</small>}
+                    <small>N/A</small>}
                     <small className='px-2 border-x border-gray-300'>{data.type}</small>
-                    <small>{data.studios[0].name}</small>
+                    <small>{data.studios.length ? data.studios[0].name : 'N/A'}</small>
 
                 </div>
             </div>
